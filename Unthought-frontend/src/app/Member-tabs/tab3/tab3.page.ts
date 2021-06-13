cbb699e9a51e4b439fe6209b6e633045e2c66b24
hrishikesh <hrishikeshnb007@gmail.com>
2021-06-13 19:29:30 +0530

    member mentor and post pages

--- Unthought-frontend/src/app/Member-tabs/tab3/tab3.page.ts
+++ Unthought-frontend/src/app/Member-tabs/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class MemberPostPage {
+  
+  form: FormGroup;
+  primary_toggle: boolean = false;
+  selected: any;
+  primary2_toggle: boolean = false;
+  selected2: any;
+  primary3_toggle: boolean = false;
+  selected3:any;
+  
+  constructor(public menuCtrl: MenuController, public alertController: AlertController,private router: Router,) {}
 
-  constructor() {}
-
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+  primarySelcted(type) {
+    this.selected = type;
+    this.primary_toggle = false;
+  }  
+  primarySelcted2(type) {
+    this.selected2 = type;
+    this.primary2_toggle = false;
+  }
+  primarySelcted3(type){
+    this.selected3 = type;
+    this.primary3_toggle = false;
+  }
 }
+
+
--- Unthought-frontend/src/app/sign-up-as/sign-up-member/sign-up-member.page.ts
+++ Unthought-frontend/src/app/sign-up-as/sign-up-member/sign-up-member.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-sign-up-member',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-member.page.scss'],
 })
 export class SignUpMemberPage implements OnInit {
+  
+  form: FormGroup;
 
-  constructor() { }
+  primary_toggle: boolean = false;
+  pri_specification_main: any;
+  pri_specification_sub: any;
+  primary_sub_toggle:  boolean = false;
+
+  secoundary_toggle: boolean = false;
+  sec_specification_main: any;
+  sec_specification_sub: any;
+  secoundary_sub_toggle:  boolean = false;
+
+  constructor(public menuCtrl: MenuController, public alertController: AlertController,private router: Router,) { }
 
   ngOnInit() {
   }
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+
+  onSubmit(form: NgForm) {
+    var postdata = {
+      unique_name: form.value.Uname,
+      Member_name: form.value.Mname,
+      gender: form.value.gender,
+      dob: form.value.dob,
+      phone_number: form.value.Uname,   
+      email: form.value.email,
+      working_status: form.value.workstatus,
+      company_name: form.value.campany,
+    }
+  }
+
+  primarySelcted(type) {
+    this.pri_specification_main = type;
+    this.primary_sub_toggle  =true;
+  }
+
+  primarysubSelcted(type) {
+    this.pri_specification_sub = type;
+    this.primary_sub_toggle = false;
+    this.primary_toggle = false;
+  }
+  
+  secoundarySelcted(type){
+    this.sec_specification_main = type;
+    this.secoundary_sub_toggle = true;  
+  }
+
+  secoundarysubSelcted(type) {
+    this.sec_specification_sub = type;
+    this.secoundary_sub_toggle = false;
+    this.secoundary_toggle = false;
+  }
+
 
 }

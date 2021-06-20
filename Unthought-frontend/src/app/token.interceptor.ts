e3aa0c2e9da0066de9bb36e4639c5ee51fbf2077
hrishikesh <hrishikeshnb007@gmail.com>
2021-06-20 15:27:24 +0530

    pulled member sign up

--- Unthought-frontend/src/app/token.interceptor.ts
+++ Unthought-frontend/src/app/token.interceptor.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
 export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     //const user = '1846b2aa521497244d79980e83cd6e07bfe4d546';
-    const user = '64bc2e8802bd9799d16e00de496b5e418787d332';
+    //const user = '64bc2e8802bd9799d16e00de496b5e418787d332';
+     const user = '14d9db934f6ab813b70e059695104da277c968d5';
     if (user) {
       request = request.clone({
         setHeaders: {
